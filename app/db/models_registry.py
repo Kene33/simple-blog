1adb9f3e0e1edf6ec948e27d1be9b95441597d04
Kene33 <gojoin14@gmail.com>
2026-07-19 12:54:27 +0300

    feat: add async PostgreSQL model foundation

---
+++ app/db/models_registry.py
@@ -0,0 +1,15 @@
+from app.db.models import Comment, Media, Post, PostLike, PostMedia, PostTag, RefreshSession, Report, ShareEvent, Tag, User
+
+__all__ = [
+    "Comment",
+    "Media",
+    "Post",
+    "PostLike",
+    "PostMedia",
+    "PostTag",
+    "RefreshSession",
+    "Report",
+    "ShareEvent",
+    "Tag",
+    "User",
+]
